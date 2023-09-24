76aa0b9ac3766296ee416b0d18e7564ad7ccbb15
Nilton Junior <14946185+niltonjr80@users.noreply.github.com>
2023-09-24 03:32:11 +0000

    Organizando estrutura de pastas

---
+++ Atividade 3/main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+
+// Função para realizar a ordenação por inserção
+void insertionSort(int arr[], int n, long long int *trocas, long long int *comparacoes)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Realiza comparações e trocas
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+            (*trocas)++;
+            (*comparacoes)++;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+// Função para realizar a ordenação por merge sort
+void merge(int arr[], int left, int middle, int right, long long int *trocas, long long int *comparacoes)
+{
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    // Cria vetores temporários para a divisão
+    int L[n1], R[n2];
+
+    // Copia os dados para os vetores temporários L[] e R[]
+    for (int i = 0; i < n1; i++)
+    {
+        L[i] = arr[left + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        R[i] = arr[middle + 1 + i];
+    }
+
+    int i = 0, j = 0, k = left;
+
+    // Realiza a mesclagem dos vetores temporários de volta para arr[]
+    while (i < n1 && j < n2)
+    {
+        // Realiza comparações e trocas
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        (*trocas)++;
+        (*comparacoes)++;
+    }
+
+    // Copia os elementos restantes de L[], se houver
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+        (*trocas)++;
+        (*comparacoes)++;
+    }
+
+    // Copia os elementos restantes de R[], se houver
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+        (*trocas)++;
+        (*comparacoes)++;
+    }
+}
+
+// Função principal
+int main()
+{
+    int Q;
+    printf("Numero Q de vetores: ");
+    scanf("%d", &Q);
+
+    for (int q = 0; q < Q; q++)
+    {
+        int N;
+        scanf("%d", &N);
+
+        int vetor[N];
+        for (int i = 0; i < N; i++)
+        {
+            scanf("%d", &vetor[i]);
+        }
+
+        long long int trocas_insertion = 0;
+        long long int comparacoes_insertion = 0;
+        long long int trocas_merge = 0;
+        long long int comparacoes_merge = 0;
+
+        // Ordena o vetor usando o método de inserção
+        int copia_insertion[N];
+        for (int i = 0; i < N; i++)
+        {
+            copia_insertion[i] = vetor[i];
+        }
+        insertionSort(copia_insertion, N, &trocas_insertion, &comparacoes_insertion);
+
+        // Ordena o vetor usando o método de merge sort
+        int copia_merge[N];
+        for (int i = 0; i < N; i++)
+        {
+            copia_merge[i] = vetor[i];
+        }
+        int left = 0;
+        int right = N - 1;
+
+        void mergeSort(int copia_merge[], int left, int right, long long int *trocas, long long int *comparacoes)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                mergeSort(copia_merge, left, middle, trocas, comparacoes);
+                mergeSort(copia_merge, middle + 1, right, trocas, comparacoes);
+                merge(copia_merge, left, middle, right, trocas, comparacoes);
+            }
+        }
+        mergeSort(copia_merge, left, right, &trocas_merge, &comparacoes_merge);
+
+        // Imprime os resultados
+        printf("I %d %lld %lld\n", N, trocas_insertion, comparacoes_insertion);
+        printf("M %d %lld %lld\n", N, trocas_merge, comparacoes_merge);
+    }
+
+    return 0;
+}
